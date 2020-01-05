91ba627f3412e8f4786fd2746dca1c365c0fc8f9
Joseph Torres <jdtorres@ucdavis.edu>
2020-01-04 17:33:22 -0800

    Basic home-login-signup routes complete

---
+++ social-media-app-client/src/pages/login.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class login extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Login Page</h1>
+      </div>
+    );
+  }
+}
+
+export default login;
