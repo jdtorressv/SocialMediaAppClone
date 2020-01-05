b0d13b6c89db1454908b9c94f7b91bc1c52e649e
Joseph Torres <jdtorres@ucdavis.edu>
2020-01-04 19:37:34 -0800

    five oh 9

--- social-media-app-client/src/pages/home.js
+++ social-media-app-client/src/pages/home.js
@@ -1,11 +1,40 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import axios from "axios";
+import Scream from "../components/Scream";
 
 class home extends React.Component {
+  state = {
+    screams: null
+  };
+  componentDidMount() {
+    axios
+      .get("/screams")
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          screams: res.data
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map((scream) => <Scream scream={scream} />)
+    ) : (
+      <p>Loading...</p>
+    );
     return (
-      <div>
-        <h1>Home Page</h1>
-      </div>
+      <Grid container spacing={10}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile coming soon...</p>
+        </Grid>
+      </Grid>
     );
   }
 }
